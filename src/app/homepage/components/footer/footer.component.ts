ce2c98a1aabbea0a33461df05733121060b7e6ad
vabshere <vabs.m.here@gmail.com>
2018-08-29 14:08:15 -0700

    adding final contact info and some changes

--- src/app/homepage/components/footer/footer.component.ts
+++ src/app/homepage/components/footer/footer.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   links = [
-    {name: 'facebook', class:'fab fa-facebook-f'},
-    {name: 'youtube', class:'fab fa-youtube'},
-    {name: 'instagram', class:'fab fa-instagram'},
-    {name: 'linked_in', class:'fab fa-linkedin-in'},
+    {name: 'facebook', class:'fab fa-facebook-f', url: 'https://www.facebook.com/antaragni.iitk/'},
+    {name: 'youtube', class:'fab fa-youtube', url: 'https://www.youtube.com/user/antaragniiitkanpur/'},
+    {name: 'instagram', class:'fab fa-instagram', url: 'https://www.instagram.com/antaragni_iitk/'},
+    {name: 'linked_in', class:'fab fa-linkedin-in', url: 'https://www.linkedin.com/company/antaragni-iit-kanpur/'},
   ]
 
   constructor() { }
