bf6521c23388dd84ad206f7e0af5973cb2193ce1
vabshere <vabs.m.here@gmail.com>
2018-08-30 13:10:10 -0700

    make firestore faster

--- src/app/homepage/components/registeration/register-dialog/register-dialog.component.ts
+++ src/app/homepage/components/registeration/register-dialog/register-dialog.component.ts
@@ -1,8 +1,9 @@
-import { RegisterationComponent } from './../registeration.component';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { Component, OnInit, Inject } from '@angular/core';
+import {RegisterationComponent} from './../registeration.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FbloginService} from '@services/fblogin.service';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-register-dialog',
@@ -11,19 +12,22 @@ import {FbloginService} from '@services/fblogin.service';
 })
 export class RegisterDialogComponent implements OnInit {
 
-  constructor(
-    public dialogRef: MatDialogRef<RegisterationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data,
-    public loginService: FbloginService, private router: Router) {}
+  constructor(private afs: AngularFirestore,
+              public dialogRef: MatDialogRef<RegisterationComponent>,
+              @Inject(MAT_DIALOG_DATA) public data,
+              public loginService: FbloginService, private router: Router) {
+  }
 
   ngOnInit() {
   }
+
   closedialog() {
     this.dialogRef.close();
   }
 
   onhit() {
-    this.loginService.isAuthenticated$.subscribe(res => res ? this.router.navigateByUrl('/dashboard') : this.loginService.signin());
+    this.afs.collection('/test').valueChanges().subscribe(() =>
+      this.loginService.isAuthenticated$.subscribe(res => res ? this.router.navigateByUrl('/dashboard') : this.loginService.signin()));
   }
 
 }
